6c89d6a5ad5ae01e9322bd6331cf2565017b48fc
Sungjun Yun <108875242+SSSSSungjun@users.noreply.github.com>
2026-07-10 13:24:21 +0900

    Accept parent comment id

--- complete/src/main/java/com/example/restservice/dto/request/CommentRequestDto.java
+++ complete/src/main/java/com/example/restservice/dto/request/CommentRequestDto.java
@@ -20,4 +20,6 @@ public class CommentRequestDto {
     @NotBlank(message = "댓글 내용을 입력해주세요.")
     @Size(max = 1000, message = "댓글은 1000자 이하로 입력해주세요.")
     private String content;
+
+    private Long parentCommentId;
 }
